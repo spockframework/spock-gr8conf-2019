d35fd76a91c5abcf6f9ddbb753b4acee573ce4d8
Leonard Brünings <leonard.bruenings@1und1.de>
2019-05-21 14:32:15 +0200

    Add FalseNegativeTest

---
+++ src/test/groovy/FalseNegativeTest.groovy
@@ -0,0 +1,58 @@
+import spock.lang.Shared
+import spock.lang.Specification
+
+
+class FalseNegativeTest extends Specification {
+
+    @Shared
+    int counter = 0
+
+    def "counter plus equals can be used"() {
+        when:
+        counter+=1
+
+        then:
+        counter == 1
+    }
+
+    def "counter plus plus"() {
+        when:
+        counter++
+
+        then:
+        counter == 2
+    }
+
+    def "counter is result of addition"() {
+        when:
+        counter = counter + 2
+
+        then:
+        counter == 4
+    }
+
+
+    def "counter minus equals can be used"() {
+        when:
+        counter-=1
+
+        then:
+        counter == 3
+    }
+
+    def "counter minus minus"() {
+        when:
+        counter--
+
+        then:
+        counter == 2
+    }
+
+    def "counter is result of subtraction"() {
+        when:
+        counter = counter - 2
+
+        then:
+        counter == 0
+    }
+}
