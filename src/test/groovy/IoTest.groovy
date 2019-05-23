aca170c62ee77cb72d997785b8adcb7b9563ce98
Leonard Brünings <leonard.bruenings@1und1.de>
2019-05-23 11:37:13 +0200

    Add IO Test

---
+++ src/test/groovy/IoTest.groovy
@@ -0,0 +1,26 @@
+import java.nio.file.Files
+
+import spock.lang.AutoCleanup
+import spock.lang.Specification
+
+class IoTest extends Specification {
+
+    @AutoCleanup('delete')
+    File parent = Files.createTempDirectory("spock-tmp").toFile()
+
+    @AutoCleanup('delete')
+    File target  = new File(parent, 'test.txt')
+
+    def setup() {
+        target.text = 'Hello World'
+    }
+
+    def "test"() {
+        when:
+        def result = target.text
+
+        then:
+        result == 'Hello World'
+    }
+
+}
